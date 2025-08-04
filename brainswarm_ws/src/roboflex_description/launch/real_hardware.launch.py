2d9f6378dcd6df51018dee124987c8372470adab
muneeb <munn33b@gmail.com>
2025-08-04 11:16:54 +0500

    position_only_ik in MoveIt + Dynamic Path in start_controllers

--- brainswarm_ws/src/roboflex_description/launch/real_hardware.launch.py
+++ brainswarm_ws/src/roboflex_description/launch/real_hardware.launch.py
@@ -5,6 +5,7 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue  # Import this
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
